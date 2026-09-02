e5835293b1d51e727a8ce2de34db5a7e5b59b74a
Mehdi-YC <mehdidouy@gmail.com>
2026-09-02 13:46:06 +0100

    allow proc to fs

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+		fs: {
+			allow: ['/proc']
+		}
+	},
 	plugins: [
 		tailwindcss(),
 		sveltekit({
